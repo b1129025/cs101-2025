9aa4b28335e8cb7f36ff0e551b594dbaedfeee66
b1129025 <120005392+b1129025@users.noreply.github.com>
2025-03-06 16:55:45 +0800

    Update main4.c

--- hw1/main4.c
+++ hw1/main4.c
@@ -1,1 +1,31 @@
-x
+#include <stdio.h>
+
+#include <string.h>
+
+int main() {
+
+    char str[100];  // 假設字串最大長度為 100
+
+    int length, i;
+
+    // 讀取字串
+
+    scanf("%s", str);
+
+    // 獲得字串的長度
+
+    length = strlen(str);
+
+    // 反向輸出字串
+
+    for(i = length - 1; i >= 0; i--) {
+
+        printf("%c", str[i]);
+
+    }
+
+    printf("\n");
+
+    return 0;
+
+}
