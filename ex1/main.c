13eab00c63092d752ee7a197fe2139f1c3a0365e
b1129025 <120005392+b1129025@users.noreply.github.com>
2025-03-06 15:56:40 +0800

    Update main.c

--- ex1/main.c
+++ ex1/main.c
@@ -2,25 +2,44 @@
 #include <stdlib.h>
 
 int main() {
-    FILE* fp;
-    int a[] = {1,2,3} ;
-    int a_read[sizeof(a)/sizeof(a[0])];
-    char b[] = "ABC" ;
-    float c[] = {1.1, 1.2, 1.3} ;
-    if ((fp = fopen("a.bin", "wb+")) == NULL) {
-        printf("Cannot open the file...") ;
-        exit(1);
-    }
-    if (fwrite(a, sizeof(a), 1,fp) != 1) {
-        printf("File write error....\n");
-    }
-    fseek(fp, 0, SEEK_SET);
+    FILE *fp;
 
-    if (fread(a_read,sizeof(a_read), 1, fp) != 1) {
-        printf("File read error....\n") ;
+    fp = fopen("a.bin", "wb");
+
+    int a[] = {0, 1, 2};
+    char b[] = "ABC";    
+    float c[] = {1.1, 1.2, 1.3};
+
+    fwrite(a, sizeof(a), 1, fp);
+    fwrite(b, sizeof(b), 1, fp);  
+    fwrite(c, sizeof(c), 1, fp);
+    fclose(fp);
+
+    fp = fopen("a.bin", "rb");
+
+    int a_read[3];
+    char b_read[4];  
+    float c_read[3];
+
+    fread(a_read, sizeof(a_read), 1, fp);
+    fread(b_read, sizeof(b_read), 1, fp);
+    fread(c_read, sizeof(c_read), 1, fp);
+    fclose(fp);
+
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", a_read[i]);
     }
-    for (int i = 0 ;i < (sizeof(a)/sizeof(a[0])) ;i++) {
-        printf("%d",a_read[i]);
+    printf("\n");
+
+    for (int i = 0; i < 3; i++) {
+        printf("%c ", b_read[i]);
     }
-    return 0 ;
+    printf("\n");
+
+    for (int i = 0; i < 3; i++) {
+        printf("%f ", c_read[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
