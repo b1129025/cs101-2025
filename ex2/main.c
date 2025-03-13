e51883109cd5950b0b56ea69baaeb89410417743
b1129025 <120005392+b1129025@users.noreply.github.com>
2025-03-13 15:48:56 +0800

    Update main.c

--- ex2/main.c
+++ ex2/main.c
@@ -1,76 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef struct employee{
-    int id;
-    int age;
+typedef struct {
+    int   id;
+    char  name[20];
+    int   age;
     float salary;
-    char name[12];
-}employee_t;
+} employee_t;
 
-void emp_info(employee_t emp){
-    printf("employee id=%d\n",emp.id);
-    printf("employee name=%s\n",emp.name);
-    printf("employee age=%d\n",emp.age);
-    printf("employee salary=%02f\n\n",emp.salary);
-}
+void emp_info(employee_t emp[], int n);
+void emp_writefile(employee_t emp[], int n, const char *filename);
+void emp_readfile(employee_t emp[], int n, const char *filename);
 
-int emp_average_age(employee_t emp[],int n){
-    int sum=0;
-    for(int i=0;i<3;i++){
-        sum+=emp[i].age;
-    }
-    return sum/3;
-}
-
-void emp_writefile(employee_t emp[],int n){
-    FILE*fp=fopen("employee.bin","wb+");
-    for(int i=0;i<n;i++){
-        fwrite(&emp[i],sizeof(employee_t),1,fp);
-    }
-    fclose(fp);
-    return;
-}
-
-void emp_readfile(employee_t emp[]){
-    FILE*fp=fopen("employee.bin","rb");
-    int i=0;
-    while(fread(&emp[i],sizeof(employee_t),1,fp)){
-        printf("[%d] %d %s\n",i,emp[i].id,emp[i].name);
-        i++;
-    }
-    fclose(fp);
-    return;
-}
-
-int main()
-{
+int main(void) {
     employee_t emp[3];
-    emp[0].id=1;
-    emp[0].age=20;
-    emp[0].salary=30000.0;
-    strcpy(emp[0].name,"IU");
 
-    emp[1].id=2;
-    emp[1].age=26;
-    emp[1].salary=36000.0;
-    strcpy(emp[1].name,"taylor");
+    emp_info(emp, 3);
 
-    emp[2].id=3;
-    emp[2].age=31;
-    emp[2].salary=90000.0;
-    strcpy(emp[2].name,"swift");
+    emp_writefile(emp, 3, "employee.bin");
 
-    int age=emp_average_age(emp,3);
-    printf("Average age:%d\n\n",age);
+    memset(emp, 0, sizeof(emp));
 
-    emp_writefile(emp,3);
-    employee_t read_emp[10];
-    emp_readfile(read_emp);
+    emp_readfile(emp, 3, "employee.bin");
 
-    for(int i=0;i<3;i++){
-        emp_info(emp[i]);
+    for(int i = 0; i < 3; i++) {
+        printf("[%d] %d %s\n", i, emp[i].id, emp[i].name);
+    }
+    printf("\n");
+
+    for(int i = 0; i < 3; i++) {
+        printf("employee id = %d\n", emp[i].id);
+        printf("employee name = %s\n", emp[i].name);
+        printf("employee age = %d\n", emp[i].age);
+        printf("employee salary = %f\n", emp[i].salary);
+        printf("\n");
     }
 
     return 0;
+}
+
+
+void emp_info(employee_t emp[], int n) {
+    emp[0].id = 1;
+    strcpy(emp[0].name, "IU");
+    emp[0].age = 20;
+    emp[0].salary = 30000.0f;
+
+    emp[1].id = 2;
+    strcpy(emp[1].name, "taylor");
+    emp[1].age = 26;
+    emp[1].salary = 36000.0f;
+
+    emp[2].id = 3;
+    strcpy(emp[2].name, "swift");
+    emp[2].age = 31;
+    emp[2].salary = 90000.0f;
+}
+
+
+void emp_writefile(employee_t emp[], int n, const char *filename) {
+    FILE *fp = fopen(filename, "wb");
+    if(!fp) {
+        perror("fopen (write)");
+        exit(EXIT_FAILURE);
+    }
+    fwrite(emp, sizeof(employee_t), n, fp);
+    fclose(fp);
+}
+
+
+void emp_readfile(employee_t emp[], int n, const char *filename) {
+    FILE *fp = fopen(filename, "rb");
+    if(!fp) {
+        perror("fopen (read)");
+        exit(EXIT_FAILURE);
+    }
+    fread(emp, sizeof(employee_t), n, fp);
+    fclose(fp);
 }
